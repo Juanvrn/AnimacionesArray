ae371e62d71b9d06955677976097509a7eb5e7ff
Juanvrn <aismer.148@gmail.com>
2025-07-06 17:38:03 -0500

    Update dataprincipal.js

--- dataprincipal.js
+++ dataprincipal.js
@@ -5,11 +5,15 @@ const menuData = [
         children: [
             { label: "ARPFortnite", type: "iframeLink", link: "arpfortnite.html" },
             { label: "Minecraft", type: "iframeLink", link: "animminecraft.html" },
-            { label: "MinecraftMobs", type: "iframeLink", link: "animmobs.html" }
+            { label: "MinecraftMobs", type: "iframeLink", link: "animmobs.html" },
         ]
     },
-    // CAMBIO AQUÍ para que "Modelos" y "Audios" también usen el iframe:   
     { label: "Modelos", type: "iframeLink", link: "modelos.html" },
-    { label: "Objetos", type: "iframeLink", link: "objetos.html" },
+    // CAMBIO AQUÍ para que "Modelos" y "Audios" también usen el iframe:   
+    { label: "Prefab", type: "dropdown", children: [
+        { label: "Objetos", type: "iframeLink", link: "objetos.html" },
+        { label: "Vehiculos", type: "iframeLink", link: "vehiculos.html" },
+    ] 
+    },
     { label: "Audios", type: "iframeLink", link: "audios.html" }
 ];
