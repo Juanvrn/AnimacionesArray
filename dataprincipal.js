3e0f819b3add7bd6fc5f99e49748421a33a27924
Juanvrn <aismer.148@gmail.com>
2025-08-08 16:19:42 -0500

    Update dataprincipal.js

--- dataprincipal.js
+++ dataprincipal.js
@@ -4,24 +4,30 @@ const menuData = [
         type: "dropdown",
         children: [
             { label: "ARPFortnite", type: "iframeLink", link: "arpfortnite.html" },
+            { label: "GTAsa", type: "iframeLink", link: "animgtasa.html" },
             { label: "Minecraft", type: "iframeLink", link: "animminecraft.html" },
             { label: "MinecraftMobs", type: "iframeLink", link: "animmobs.html" },
         ]
     },
-    { label: "Modelos", type: "dropdown", children:[
-        {label:"RigFull", type:"iframeLink", link:"modelos.html"},
-        {label:"Relleno", type:"iframeLink", link:"modelosRelleno.html"},
-    ]},
-    // CAMBIO AQUÍ para que "Modelos" y "Audios" también usen el iframe:   
-    { label: "Prefab", type: "dropdown", children: [
-        { label: "Objetos", type: "iframeLink", link: "objetos.html" },
-        { label: "Vehiculos", type: "iframeLink", link: "vehiculos.html" },
-        { label: "Escenas", type:"iframeLink", link:"escenas.html"}
-    ] 
+    {
+        label: "Modelos", type: "dropdown", children: [
+            { label: "RigFull", type: "iframeLink", link: "modelos.html" },
+            { label: "Relleno", type: "iframeLink", link: "modelosRelleno.html" },
+        ]
     },
-    { label: "Sonidos", type: "dropdown", children:[
-        {label:"GTASA", type:"iframeLink", link:"audiogta.html"},
-        {label:"GTASA Modelos", type:"iframeLink", link:"audiomodelo.html"},
-        {label:"GTASA Conversacion", type:"iframeLink", link:"audioConversacion.html"},
-    ] }
+    // CAMBIO AQUÍ para que "Modelos" y "Audios" también usen el iframe:   
+    {
+        label: "Prefab", type: "dropdown", children: [
+            { label: "Objetos", type: "iframeLink", link: "objetos.html" },
+            { label: "Vehiculos", type: "iframeLink", link: "vehiculos.html" },
+            { label: "Escenas", type: "iframeLink", link: "escenas.html" }
+        ]
+    },
+    {
+        label: "Sonidos", type: "dropdown", children: [
+            { label: "GTASA", type: "iframeLink", link: "audiogta.html" },
+            { label: "GTASA Modelos", type: "iframeLink", link: "audiomodelo.html" },
+            { label: "GTASA Conversacion", type: "iframeLink", link: "audioConversacion.html" },
+        ]
+    }
 ];
