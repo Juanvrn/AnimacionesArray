e48eeaf1bf1e40ab3b5a7cfc1fed4f21cf54075f
Juanvrn <aismer.148@gmail.com>
2025-08-08 16:16:39 -0500

    Create animgtasaData.js

---
+++ animgtasaData.js
@@ -0,0 +1,43 @@
+// Array para el primer grupo de checkboxes
+let arrayPropiedad = [
+    {
+        nombre: "Dead Island",
+        data: [
+            {
+                subpropiedad: "Air (Golpes para mandarlos a volar)", carpetas: [
+                    { id: "eeeaXU1B", valor: "Base Hit Air Adelante Inicio GTAsa" },
+                    { id: "o*gB5ypC", valor: "Base Hit Air Adelante Loop Fast GTAsa" },
+                    { id: "F5D9;*1D", valor: "Base Hit Air Adelante Loop Slow GTAsa" },
+                ]
+            },
+        ]
+    },
+];
+
+let arrayBusqueda = [
+    /*{ nombre: "Head", data: ["Cabeza", "Craneo", "Head"] },
+    { nombre: "Talk", data: ["Talk", "Chat", "Say", "Converse", "Dialogue", "Conversando", "Hablar", "Talking"] },
+    { nombre: "Fly", data: ["Vuelo", "Aire", "Fly", "Flying", "Air", "Fly", "Float", "Salto", "Aéreo", "Volar"] },
+    { nombre: "Idle", data: ["Idle", "Quieto", "Reposo", "Standby", "Inactivo", "Reposo"] },
+    { nombre: "Weapon", data: ["Weapon", "Arma", "Pistola", "Escopeta", "Metralleta", "Cuchillo", "Rifle"] },
+    { nombre: "Interact", data: ["Interact", "Objeto", "Arma", "Escopeta", "Metralleta", "Cuchillo", "Pistola", "Use", "Activate", "Grab", "Press", "Manipulate", "Interactuar", "Usar", "Tocar", "Activar", "Bate"] },
+    { nombre: "Hit", data: ["Golpear", "ATK", "Finish", "Pow", "Attack", "Patada", "Hit", "Strike", "Impact", "Punch", "Golpe", "Impacto", "Puñetazo", "Strike", "Smash", "Impact"] },
+    { nombre: "Walk", data: ["Walk", "Caminar", "Caminata", "Andar", "Pasear"] },
+    { nombre: "Run", data: ["Run", "Sprint", "Dash", "Sprint", "Dash", "Rush", "Correr", "Trotar", "Apresurarse"] },
+    { nombre: "Recovery", data: ["Recovery", "Heal", "Regain", "Restore", "GetUp", "StandUp", "Recuperación", "Sanar", "Levantarse", "Heal", "Regain", "GetUp"] },
+    { nombre: "Stun", data: ["Stun", "Daze", "Knockback", "Freeze", "Dizzy", "Paralyze", "Aturdir", "Tambalear", "Desorientar", "Dazed", "Knockback", "Stagger"] },
+    { nombre: "Combat", data: ["Combat", "Fight", "Battle", "Melee", "Brawl", "Engage", "Combate", "Pelea", "Fight", "Battle", "Engage", "Combate", "Pelea", "Lucha"] },
+    { nombre: "Death", data: ["Death", "Die", "Dead", "Kill", "Finish", "Downed", "Muerte", "Caer", "Fallecer", "Die", "Fall", "Collapse", "Perish"] },
+    { nombre: "Combos", data: ["Combos", "Chains", "Sequences", "Attacks", "Move Set", "Combo", "Chain"] },
+    { nombre: "Shoot", data: ["Shoot", "Fire", "Blast", "Gun", "Aim", "Disparar", "Blast", "Gunfire"] },
+    { nombre: "Climb", data: ["Climb", "Scale", "Ascend", "Mount", "CrawlUp", "Escalar", "Trepar", "Subir"] },
+    { nombre: "GetShot", data: ["GetShot", "TakeHit", "Damaged", "HitReaction", "ImpactReceived", "Recibir Disparo", "Ser golpeado", "Herido", "Hit", "TakeDamage", "Injured", "Impact"] },
+    { nombre: "Emotes", data: ["Expressions", "Gestures", "Reactions", "Gestos", "Emociones", "Reacciones"] },
+    { nombre: "Dance", data: ["Dance", "Move", "Groove", "Boogie", "Bailar", "Moverse", "Danza", "Dancing", "dnce"] },
+    { nombre: "Launch", data: ["Lanzar", "Impulsar", "Arrojar", "Throw", "Propel", "Toss"] },
+    { nombre: "Loop", data: ["Bucle", "Ciclo", "Repetición", "Cycle", "Repeat", "Looping", "Loop"] },
+    { nombre: "Zombie", data: ["Zombie", "Zombi", "Muertos"] },
+    { nombre: "Vehiculo", data: ["Accidente", "Carro", "Atropello", "Bus", "Driver", "Taxi"] },
+    { nombre: "Land", data: ["Land", "Suelo", "Piso", "Tierra"] },
+    { nombre: "Listen", data: ["Listen", "Escuchar", "Oir", "Oido"] }*/
+];
